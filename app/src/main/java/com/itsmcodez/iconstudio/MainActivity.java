9cc3a61b3d01a3917f3dfb5bb54a0bca92957778
Solomon Mensah <iam.itsmcodez@gmail.com>
2024-10-23 12:14:17 +0000

    [MainActivity] add suggestions to AutoCompleteTextView

--- app/src/main/java/com/itsmcodez/iconstudio/MainActivity.java
+++ app/src/main/java/com/itsmcodez/iconstudio/MainActivity.java
@@ -1,12 +1,22 @@
 
 package com.itsmcodez.iconstudio;
 
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.itsmcodez.iconstudio.databinding.ActivityMainBinding;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private String[] iconNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,50 @@ public class MainActivity extends AppCompatActivity {
 
         // set content view to binding's root
         setContentView(binding.getRoot());
+        
+        // retrieve icon names
+        try {
+            iconNames = getAssets().list("icon-packs/material-icons/");
+        } catch(IOException err) { err.printStackTrace(); }
+        
+        binding.textField.setOnEditorActionListener((textView, actionId, keyEvent) -> {
+                boolean actionHandled = false;
+                if(actionId == EditorInfo.IME_ACTION_GO || actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    if(!TextUtils.isEmpty(textView.getText())) {
+                    	// TODO: match input to names of items in recyclerview
+                    }
+                    // hide soft keyboard on input submission
+                    InputMethodManager imm = (InputMethodManager) getSystemService(AppCompatActivity.INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(binding.textField.getWindowToken(), 0);
+                	actionHandled = true;
+                }
+                return actionHandled;
+        });
+        
+        binding.textField.addTextChangedListener(new TextWatcher() {
+                @Override
+                public void onTextChanged(CharSequence s, int start, int before, int count) {
+                    //TODO: implement this method
+                }
+                
+                @Override
+                public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                    // TODO: Implement this method
+                }
+                
+                @Override
+                public void afterTextChanged(Editable s) {
+                    String input = s.toString().toLowerCase();
+                    List<String> matchCases = new ArrayList<>();
+                    for(String iconName : iconNames) {
+                    	if(iconName.contains(input)) {
+                    		matchCases.add(iconName);
+                    	}
+                    }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1, matchCases);
+                    binding.textField.setAdapter(arrayAdapter);
+                }
+        });
     }
     
     @Override
